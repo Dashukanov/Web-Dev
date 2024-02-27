aa86a8c25dd264cf59d3be58842ce6646de325ae
Dastan Shukanov <156771986+Dashukanov@users.noreply.github.com>
2024-02-27 23:48:19 +0600

    lab5

---
+++ Lab5/src/app/products.ts
@@ -0,0 +1,363 @@
+export const products = [
+  {
+    id: 1,
+    name: 'Смартфон Apple iPhone 15 Pro Max 256Gb серый',
+    price: 631490 ,
+    description: 'технология NFC: Да\n' +
+      'цвет: серый\n' +
+      'тип экрана: OLED, Super Retina XDR display\n' +
+      'диагональ: 6.7 дюйм\n' +
+      'размер оперативной памяти: 8 ГБ\n' +
+      'процессор: 6-ядерный Apple A17 Pro',
+    rating:4.2,
+    imageSrc: 'https://resources.cdn-kaspi.kz/img/m/p/hc1/h65/83559848181790.png?format=gallery-large',
+    kaspiLink: 'https://kaspi.kz/shop/p/apple-iphone-15-pro-max-256gb-seryi-113138420/?c=750000000',
+    prodcategory: "1",
+    shows: true,
+    likes:0
+  },
+  {
+    id: 2,
+    name: 'Смартфон Apple iPhone 13 128Gb Midnight черный',
+    price: 294534,
+    description: 'технология NFC: Да\n' +
+      'цвет: черный\n' +
+      'тип экрана: OLED, Super Retina XDR\n' +
+      'диагональ: 6.1 дюйм\n' +
+      'размер оперативной памяти: 4 ГБ\n' +
+      'процессор: 6-ядерный Apple A15 Bionic',
+    rating:4.1,
+    imageSrc: 'https://resources.cdn-kaspi.kz/img/m/p/h32/h70/84378448199710.jpg?format=gallery-large',
+    kaspiLink: 'https://kaspi.kz/shop/p/apple-iphone-13-128gb-midnight-chernyi-102298404/?c=750000000',
+    prodcategory: 1,
+    shows: true,
+    likes:0
+  },
+  {
+    id: 3,
+    name: 'Смартфон Apple iPhone 13 128Gb белый',
+    price: 297780,
+    description: 'технология NFC: Да\n' +
+      'цвет: белый\n' +
+      'тип экрана: OLED, Super Retina XDR\n' +
+      'диагональ: 6.1 дюйм\n' +
+      'размер оперативной памяти: 4 ГБ\n' +
+      'процессор: 6-ядерный Apple A15 Bionic',
+    rating:4,
+    imageSrc: 'https://resources.cdn-kaspi.kz/img/m/p/hc9/h90/64209083007006.jpg?format=gallery-large',
+    kaspiLink: 'https://kaspi.kz/shop/p/apple-iphone-13-128gb-belyi-102298420/?c=750000000',
+    prodcategory: 1,
+    shows: true,
+    likes:0
+  },
+  {
+    id: 4,
+    name: 'Смартфон Apple iPhone 11 128Gb Slim Box черный',
+    price: 235000,
+    description: 'технология NFC: Да\n' +
+      'цвет: черный\n' +
+      'тип экрана: сенсорный мультитач цветной IPS, Liquid Retina HD\n' +
+      'диагональ: 6.1 дюйм\n' +
+      'размер оперативной памяти: 4 ГБ\n' +
+      'процессор: 6-ядерный Apple A13 Bionic',
+    rating:4.3,
+    imageSrc: 'https://resources.cdn-kaspi.kz/img/m/p/hd8/hac/63897052413982.jpg?format=gallery-large',
+    kaspiLink: 'https://kaspi.kz/shop/p/apple-iphone-11-128gb-slim-box-chernyi-100692388/?c=750000000',
+    prodcategory: 1,
+    shows: true,
+    likes:0
+  },
+  {
+    id: 5,
+    name: 'Смартфон Poco X6 Pro 12 ГБ/512 ГБ черный',
+    price: 166038,
+    description: 'технология NFC: Да\n' +
+      'цвет: черный\n' +
+      'тип экрана: AMOLED, сенсорный, Corning Gorilla Glass 5\n' +
+      'диагональ: 6.67 дюйм\n' +
+      'размер оперативной памяти: 12 ГБ',
+    rating:4.0,
+    imageSrc: 'https://resources.cdn-kaspi.kz/img/m/p/hbe/h45/84940376899614.jpg?format=gallery-large',
+    kaspiLink: 'https://kaspi.kz/shop/p/poco-x6-pro-12-gb-512-gb-chernyi-115961596/?c=750000000',
+    prodcategory: 1,
+    shows: true,
+    likes:0
+  },
+  {
+    id: 6,
+    name: 'Смартфон Xiaomi Redmi Note 12 6 ГБ/128 ГБ серый',
+    price: 77299,
+    description: 'технология NFC: Нет\n' +
+      'цвет: черный\n' +
+      'тип экрана: AMOLED, сенсорный, Corning Gorilla Glass 3\n' +
+      'диагональ: 6.67 дюйм\n' +
+      'размер оперативной памяти: 6 ГБ',
+    rating:4.2,
+    imageSrc: 'https://resources.cdn-kaspi.kz/img/m/p/hca/h10/79755729600542.jpg?format=gallery-large',
+    kaspiLink: 'https://kaspi.kz/shop/p/xiaomi-redmi-note-12-6-gb-128-gb-seryi-109734268/?c=750000000',
+    prodcategory: 1,
+    shows: true,
+    likes:0
+  },
+  {
+    id: 7,
+    name: 'Смартфон Samsung Galaxy A24 6 ГБ/128 ГБ бордовый',
+    price: 89695,
+    description: 'технология NFC: Да\n' +
+      'цвет: бордовый\n' +
+      'тип экрана: Super AMOLED, сенсорный\n' +
+      'диагональ: 6.5 дюйм\n' +
+      'размер оперативной памяти: 6 ГБ\n' +
+      'процессор: 8-ядерный MediaTek Helio G99',
+    rating:4.0,
+    imageSrc: 'https://resources.cdn-kaspi.kz/img/m/p/h3a/hec/80750150844446.jpg?format=gallery-large',
+    kaspiLink: 'https://kaspi.kz/shop/p/samsung-galaxy-a24-6-gb-128-gb-bordovyi-110343527/?c=750000000',
+    prodcategory: 1,
+    shows: true,
+    likes:0
+  },
+  {
+    id: 8,
+    name: 'Телевизор LG 43UP76006LC 109 см черный',
+    price: 127982,
+    description: 'тип: LED-телевизор\n' +
+      'диагональ: 43.0 дюйм\n' +
+      'разрешение: 3840x2160\n' +
+      'технология Smart TV: Да',
+    rating:4.5,
+    imageSrc: 'https://resources.cdn-kaspi.kz/img/m/p/h39/h73/64332873957406.jpg?format=gallery-large',
+    kaspiLink: 'https://kaspi.kz/shop/p/lg-43up76006lc-109-sm-chernyi-101585668/?c=750000000',
+    prodcategory: 2,
+    shows: true,
+    likes:0
+  },
+  {
+    id: 9,
+    name: 'Телевизор Yasin 43G11 109 см черный',
+    price: 454,
+    description: 'тип: LED-телевизор\n' +
+      'диагональ: 43.0 дюйм\n' +
+      'разрешение: 1366x768\n' +
+      'поддержка HD: 1080p Full HD\n' +
+      'технология Smart TV: Да\n' +
+      'входы: AV, ,HDMI, ,USB, ,Bluetooth',
+    rating:4.5,
+    imageSrc: 'https://resources.cdn-kaspi.kz/img/m/p/had/h96/68005513363486.jpg?format=gallery-large',
+    kaspiLink: 'https://kaspi.kz/shop/p/yasin-43g11-109-sm-chernyi-108494502/?c=750000000',
+    prodcategory: 2,
+    shows: true,
+    likes:0
+  },
+  {
+    id: 10,
+    name: 'Телевизор Samsung UE43T5300AUXCE 109 см черный',
+    price: 121089,
+    description: 'тип: LED-телевизор\n' +
+      'диагональ: 43.0 дюйм\n' +
+      'разрешение: 1920x1080\n' +
+      'поддержка HD: 1080p Full HD\n' +
+      'технология Smart TV: Да\n' +
+      'wi-Fi: Да',
+    rating:4.4,
+    imageSrc: 'https://resources.cdn-kaspi.kz/img/m/p/h70/hca/63880820457502.jpg?format=gallery-large',
+    kaspiLink: 'https://kaspi.kz/shop/p/samsung-ue43t5300auxce-109-sm-chernyi-100182013/?c=750000000',
+    prodcategory: 2,
+    shows: true,
+    likes:0
+  },
+  {
+    id: 11,
+    name: 'Телевизор Xiaomi L32M7-EARU 81 см черный',
+    price: 84981,
+    description: 'тип: LED-телевизор\n' +
+      'диагональ: 32.0 дюйм\n' +
+      'разрешение: 1366x768\n' +
+      'поддержка HD: 720p HD\n' +
+      'технология Smart TV: Да',
+    rating:4.5,
+    imageSrc: 'https://resources.cdn-kaspi.kz/img/m/p/h93/hfd/69218658058270.jpg?format=gallery-large',
+    kaspiLink: 'https://kaspi.kz/shop/p/xiaomi-l32m7-earu-81-sm-chernyi-108977524/?c=750000000',
+    prodcategory: 2,
+    shows: true,
+    likes:0
+  },
+  {
+    id: 12,
+    name: 'Телевизор LG 43UQ76003LD 109 см черный',
+    price: 119276,
+    description: 'тип: LED-телевизор\n' +
+      'диагональ: 43.0 дюйм\n' +
+      'разрешение: 3840x2160\n' +
+      'поддержка HD: 4K UHD\n' +
+      'технология Smart TV: Да',
+    rating:4.5,
+    imageSrc: 'https://resources.cdn-kaspi.kz/img/m/p/h87/h31/64481112784926.jpg?format=gallery-large',
+    kaspiLink: 'https://kaspi.kz/shop/p/lg-43uq76003ld-109-sm-chernyi-105572393/?c=750000000',
+    prodcategory: 2,
+    shows: true,
+    likes:0
+  },
+  {
+    id: 13,
+    name: 'Ноутбук Apple MacBook Air 13 MGN93 серебристый',
+    price: 431902,
+    description: 'диагональ экрана: 13.3 дюйм\n' +
+      'процессор: Apple M1\n' +
+      'видеокарта: Apple M1\n' +
+      'размер оперативной памяти: 8.0 Гб\n' +
+      'тип жесткого диска: SSD',
+    rating:4.5,
+    imageSrc: 'https://resources.cdn-kaspi.kz/img/m/p/h9a/h35/64082972704798.jpg?format=gallery-large',
+    kaspiLink: 'https://kaspi.kz/shop/p/apple-macbook-air-13-mgn93-serebristyi-100798912/?c=750000000',
+    prodcategory: 3,
+    shows: true,
+    likes:0
+  },
+  {
+    id: 14,
+    name: 'Ноутбук Chuwi HeroBook Pro 14.1 B08316YSKH серебристый',
+    price: 119990,
+    description: 'диагональ экрана: 14.1 дюйм\n' +
+      'процессор: Intel Celeron N4020\n' +
+      'видеокарта: Intel UHD Graphics 600\n' +
+      'размер оперативной памяти: 8.0 Гб\n' +
+      'тип жесткого диска: SSD',
+    rating:4.6,
+    imageSrc: 'https://resources.cdn-kaspi.kz/img/m/p/hfd/hed/64321380974622.jpg?format=gallery-large',
+    kaspiLink: 'https://kaspi.kz/shop/p/chuwi-herobook-pro-14-1-b08316yskh-serebristyi-101675028/?c=750000000',
+    prodcategory: 3,
+    shows: true,
+    likes:0
+  },
+  {
+    id: 15,
+    name: 'Ноутбук Lenovo IdeaPad 3 15IGL05 81WQ00EMRK серый',
+    price: 168035,
+    description: 'диагональ экрана: 15.6 дюйм\n' +
+      'процессор: Intel Celeron N4020\n' +
+      'видеокарта: Intel UHD Graphics 600\n' +
+      'размер оперативной памяти: 4.0 Гб\n' +
+      'тип жесткого диска: SSD\n' +
+      'общий объем накопителей: 256.0 Гб',
+    rating:5,
+    imageSrc: 'https://resources.cdn-kaspi.kz/img/m/p/h89/haa/64373052571678.jpg?format=gallery-large',
+    kaspiLink: 'https://kaspi.kz/shop/p/lenovo-ideapad-3-15igl05-81wq00emrk-seryi-102878742/?c=750000000',
+    prodcategory: 3,
+    shows: true,
+    likes:0
+  },
+  {
+    id: 16,
+    name: 'Ноутбук HP 15s-fq0017ci 9R257EA темно-серый',
+    price: 159810,
+    description: 'диагональ экрана: 15.6 дюйм\n' +
+      'процессор: Intel Celeron N4120\n' +
+      'видеокарта: Intel UHD Graphics 600\n' +
+      'размер оперативной памяти: 8.0 Гб\n' +
+      'тип жесткого диска: SSD',
+    rating:4.6,
+    imageSrc: 'https://resources.cdn-kaspi.kz/img/m/p/hc2/h4e/84818607276062.jpg?format=gallery-large',
+    kaspiLink: 'https://kaspi.kz/shop/p/hp-15s-fq0017ci-9r257ea-temno-seryi-115752629/?c=750000000',
+    prodcategory: 3,
+    shows: true,
+    likes:0
+  },
+  {
+    id: 17,
+    name: 'Ноутбук Chuwi GemiBook XPro 8G/256G серый',
+    price: 139990,
+    description: 'диагональ экрана: 14.1 дюйм\n' +
+      'процессор: Intel N100\n' +
+      'видеокарта: Intel UHD Graphics\n' +
+      'размер оперативной памяти: 8.0 Гб\n' +
+      'тип жесткого диска: SSD\n' +
+      'общий объем накопителей: 256.0 Гб',
+    rating:4.4,
+    imageSrc: 'https://resources.cdn-kaspi.kz/img/m/p/hc4/h7e/81272592072734.jpg?format=gallery-large',
+    kaspiLink: 'https://kaspi.kz/shop/p/chuwi-gemibook-xpro-8g-256g-seryi-110855868/?c=750000000',
+    prodcategory: 3,
+    shows: true,
+    likes:0
+  },
+  {
+    id: 18,
+    name: 'Наушники Apple AirPods 2nd generation белый',
+    price: 49839,
+    description: 'тип: гарнитура\n' +
+      'вид: внутриканальные\n' +
+      'подключение: беспроводное\n' +
+      'тип акустического оформления: открытые\n' +
+      'тип крепления: без крепления',
+    rating:3,
+    imageSrc: 'https://resources.cdn-kaspi.kz/img/m/p/h12/hc8/84594232688670.jpg?format=gallery-large',
+    kaspiLink: 'https://kaspi.kz/shop/p/apple-airpods-2nd-generation-belyi-114927908/?c=750000000',
+    prodcategory: 4,
+    shows: true,
+    likes:0
+  },
+  {
+    id: 19,
+    name: 'Наушники Realme Buds T100 черный',
+    price: 9383,
+    description: 'тип: гарнитура\n' +
+      'вид: внутриканальные\n' +
+      'подключение: беспроводное\n' +
+      'тип акустического оформления: закрытые\n' +
+      'тип крепления: без крепления',
+    rating:4.1,
+    imageSrc: 'https://resources.cdn-kaspi.kz/img/m/p/h70/h1a/65147416477726.jpg?format=gallery-large',
+    kaspiLink: 'https://kaspi.kz/shop/p/realme-buds-t100-chernyi-107357596/?c=750000000',
+    prodcategory: 4,
+    shows: true,
+    likes:0
+  },
+  {
+    id: 20,
+    name: 'Наушники Marshall Major IV черный',
+    price: 66427,
+    description: 'тип: гарнитура\n' +
+      'вид: накладные\n' +
+      'подключение: беспроводное\n' +
+      'тип акустического оформления: закрытые\n' +
+      'тип крепления: оголовье',
+    rating:4.2,
+    imageSrc: 'https://resources.cdn-kaspi.kz/img/m/p/h20/h21/64344048762910.jpg?format=gallery-large',
+    kaspiLink: 'https://kaspi.kz/shop/p/marshall-major-iv-chernyi-102138144/?c=750000000',
+    prodcategory: 4,
+    shows: true,
+    likes:0
+  },
+  {
+    id: 21,
+    name: 'Наушники CAT EAR STN-28 розовый',
+    price: 1898,
+    description: 'тип: гарнитура\n' +
+      'вид: накладные\n' +
+      'подключение: беспроводное\n' +
+      'тип акустического оформления: закрытые\n' +
+      'тип крепления: оголовье',
+    rating:3.7,
+    imageSrc: 'https://resources.cdn-kaspi.kz/img/m/p/h46/hd1/64251276197918.jpg?format=gallery-large',
+    kaspiLink: 'https://kaspi.kz/shop/p/cat-ear-stn-28-rozovyi-101768468/?c=750000000',
+    prodcategory: 4,
+    shows: true,
+    likes:0
+  },
+  {
+    id: 22,
+    name: 'Наушники Samsung Galaxy Buds2 Pro (SM-R510NZAACIS) черный',
+    price: 55065,
+    description: 'тип: гарнитура\n' +
+      'вид: внутриканальные\n' +
+      'подключение: беспроводное\n' +
+      'тип акустического оформления: закрытые\n' +
+      'тип крепления: без крепления\n' +
+      'система активного шумоподавления: Да',
+    rating:4.6,
+    imageSrc: 'https://resources.cdn-kaspi.kz/img/m/p/hc2/h75/64421533548574.jpg?format=gallery-large',
+    kaspiLink: 'https://kaspi.kz/shop/p/samsung-galaxy-buds2-pro-sm-r510nzaacis-chernyi-106128763/?c=750000000',
+    prodcategory: 4,
+    shows: true,
+    likes:0
+  }
+];
